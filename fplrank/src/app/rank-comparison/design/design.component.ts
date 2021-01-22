47683207f71401f6d637c21ea3739d963ebbc11f
Nicky Bonello <nbonelloghio@hotmail.com>
2021-01-22 23:20:07 +0100

    add color pallet to graph

--- fplrank/src/app/rank-comparison/design/design.component.ts
+++ fplrank/src/app/rank-comparison/design/design.component.ts
@@ -1,12 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartDataSets } from 'chart.js';
 import { Label } from 'ng2-charts';
-import { multi } from 'src/app/data';
-import { FplPlayer } from 'src/app/models/PlayerRank';
-import { Series } from "src/app/models/Series";
-import { FplApiService } from 'src/app/services/FplApiService';
 
-import {Node} from 'src/app/models/Node';
 
 @Component({
   selector: 'app-design',
@@ -18,10 +13,6 @@ export class DesignComponent implements OnInit {
 
   constructor(){}
 
-  playerIds: number[] = [];
-  players: Node[] = [];
-  multi: any[];
-  playerId: number = 1;
 
   lineChartData: ChartDataSets[] = [
     {
@@ -73,88 +64,8 @@ export class DesignComponent implements OnInit {
   lineChartPlugins = [];
   lineChartType = 'line';
 
-  async getLeague() {
-    this.playerIds = await this.api.GetLeaguePlayerDetails(163980);
-
-    console.log(this.playerIds);
-  }
-
-  
-
-  addPlayerId() {
-    if (!this.playerIds.includes(this.playerId)) {
-      console.log('Adding player to playerIds:' + this.playerId);
-      this.playerIds.push(this.playerId);
-    }
-
-    //Reset input
-    this.playerId = 1;
-  }
-
-  async plotGraph() {
-    var promiseArray = [];
-    for await (var playerId of this.playerIds) {
-      console.log('Will plot: ' + playerId);
-
-      if (
-        this.players.find((p) => p.name == playerId.toString()) === undefined
-      ) {
-        console.log('Getting player from FPL : ' + playerId);
-        promiseArray.push(this.api.GetPlayerDetails(playerId));
-      } else {
-        console.log('Skipping player:' + playerId);
-      }
-    }
-
-    var fplPlayers = await Promise.all(promiseArray);
-    console.log(fplPlayers);
-    fplPlayers.forEach((player) => {
-      if (this.players.find((p) => p.name == player.name) == undefined)
-        this.addNewPlayer(player);
-    });
-
-    console.log(this.players);
-    this.multi = JSON.parse(JSON.stringify(this.players));
-    console.log(this.multi);
-  }
-
-  addNewPlayer(player: FplPlayer) {
-    this.players.push(
-      new Node(
-        player.name,
-        player.scores.map((f) => new Series(f.total_points, f.event.toString()))
-      )
-    );
-
-    var x = this.lineChartData.find((p) => p.label == player.id.toString());
-    console.log(x);
-    if (x == undefined) {
-      var xx: ChartDataSets = {
-        label: player.id.toString(),
-        data: player.scores.map((s) => s.total_points),
-        pointHoverRadius: 3,
-        fill: false,
-        backgroundColor: "red",
-        pointHoverBackgroundColor : "green",
-        
-      };
-
-      this.lineChartData.push(xx);
-    } else {
-      player.scores.forEach((score) => {
-        this.lineChartData
-          .find((p) => p.label == player.id.toString())
-          .data.push(score.total_points);
-      });
-    }
-  }
-
-  getRandomColor() {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+  updateDataset($event){
+    console.log($event);
+    this.lineChartData = $event;
   }
 }
--- fplrank/src/app/rank-comparison/rank-edit/rank-edit/rank-edit.component.ts
+++ fplrank/src/app/rank-comparison/rank-edit/rank-edit/rank-edit.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FplPlayer } from 'src/app/models/PlayerRank';
 import { Node } from 'src/app/models/Node';
 import { FplApiService } from 'src/app/services/FplApiService';
+import { ChartDataSets } from 'chart.js';
+import { Series } from 'src/app/models/Series';
+import { ColorHelper } from '@swimlane/ngx-charts';
 
 
 @Component({
-  selector: 'app-rank-edit',
+  selector: 'rank-edit',
   templateUrl: './rank-edit.component.html',
   styleUrls: ['./rank-edit.component.scss']
 })
@@ -15,13 +18,28 @@ export class RankEditComponent implements OnInit {
   playerIds: number[] = [];
   players: Node[] = [];
   playerId: number = 1;
+  lineChartData: ChartDataSets[] = [];
+  lineChartColors = [
+    '#791c81',
+    '#43328d',
+    '#416fb8',
+    '#519db8',
+    '#70b484',
+    '#99bd5c',
+    '#c3bb45',
+    '#e0a339',
+    '#e66b2d',
+    '#d92020'
+  ]
 
+
+  @Output() dataEvent = new EventEmitter<ChartDataSets[]>();
+  
   constructor(private api: FplApiService) {
   }
 
   ngOnInit(): void {
   }
-
 
   async getLeague() {
     this.playerIds = await this.api.GetLeaguePlayerDetails(163980);
@@ -44,8 +62,6 @@ export class RankEditComponent implements OnInit {
   async plotGraph() {
     var promiseArray = [];
     for await (var playerId of this.playerIds) {
-      console.log('Will plot: ' + playerId);
-
       if (
         this.players.find((p) => p.name == playerId.toString()) === undefined
       ) {
@@ -57,13 +73,11 @@ export class RankEditComponent implements OnInit {
     }
 
     var fplPlayers = await Promise.all(promiseArray);
-    console.log(fplPlayers);
     fplPlayers.forEach((player) => {
       if (this.players.find((p) => p.name == player.name) == undefined)
         this.addNewPlayer(player);
     });
 
-    console.log(this.players);
   }
 
   addNewPlayer(player: FplPlayer) {
@@ -74,17 +88,20 @@ export class RankEditComponent implements OnInit {
       )
     );
 
-    var x = this.lineChartData.find((p) => p.label == player.id.toString());
+    var x = this.lineChartData.find(p => p.label == player.id.toString());
+    var position = this.lineChartData.findIndex(p => p.label == player.id.toString());
+
+    console.log(position)
     console.log(x);
+    console.log(this.lineChartData)
+
     if (x == undefined) {
       var xx: ChartDataSets = {
         label: player.id.toString(),
         data: player.scores.map((s) => s.total_points),
-        pointHoverRadius: 3,
-        fill: false,
-        backgroundColor: "red",
-        pointHoverBackgroundColor : "green",
-        
+        borderColor : this.lineChartColors[this.lineChartData.length],
+        pointBackgroundColor : this.lineChartColors[this.lineChartData.length],
+        fill : false
       };
 
       this.lineChartData.push(xx);
@@ -95,6 +112,8 @@ export class RankEditComponent implements OnInit {
           .data.push(score.total_points);
       });
     }
+
+    this.dataEvent.emit(this.lineChartData);
   }
 
 }
